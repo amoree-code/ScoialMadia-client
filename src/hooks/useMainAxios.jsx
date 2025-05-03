460108d9c2778f0d92d5d223e31e42a214e36b08
amoree-code <amoreevw@gmail.com>
2025-05-03 14:07:07 +0300

    baseUrl

--- src/hooks/useMainAxios.jsx
+++ src/hooks/useMainAxios.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  // baseURL: "https://server-test-production-5225.up.railway.app",
-  baseURL: "http://localhost:4777",
+  baseURL: "https://server-test-production-5225.up.railway.app",
+  // baseURL: "http://localhost:4777",
 
   headers: {
     "Content-Type": "application/json",
